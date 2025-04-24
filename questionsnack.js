2f5d49ab3421bce8ead92bda1211706fa3899ab7
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-24 17:47:08 +0200

    Add chef object with methods and nested restaurant structure for deep copy functionality

--- questionsnack.js
+++ questionsnack.js
@@ -4,3 +4,24 @@
 
 // ⚠️ Serve usare una funzione ricorsiva! (fai un po’ di ricerca).
 
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
